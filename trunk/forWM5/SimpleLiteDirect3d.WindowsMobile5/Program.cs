d69a3bb35437676bcfbac2a1ae0ca61dfdf8cba7
nyatla <nyatla@7cac0a50-4618-4814-88d0-24b83990f816>
2008-10-13 08:37:56 +0000

    [バックアップ]NyARToolkitCS WM用サンプルプログラム修正
    git-svn-id: https://svn.sourceforge.jp/svnroot/nyartoolkit/NyARToolkitCS@194 7cac0a50-4618-4814-88d0-24b83990f816

--- trunk/forWM5/SimpleLiteDirect3d.WindowsMobile5/Program.cs
+++ trunk/forWM5/SimpleLiteDirect3d.WindowsMobile5/Program.cs
@@ -46,63 +46,49 @@ namespace SimpleLiteDirect3d.WindowsMobile5
             // フォームとメインサンプルクラスを作成
             using (NyARToolkitCS frm = new NyARToolkitCS())
             {
-                DeviceAdapter dev_adapter;
-                //デバイスを選択して作る。
-                using (Form2 frm2 = new Form2())
+                ResourceBuilder nyar_res;
+                try
                 {
-                    frm2.ShowDialog();
-                    dev_adapter = frm2.GetSelectedDeviceAdapter();
+                    nyar_res = new ResourceBuilder();
                 }
-                //デバイス作れなかったらおしまい。
-                if (dev_adapter == null)
+                catch (Exception e)
                 {
+                    MessageBox.Show(e.Message, "SimpleLiteD3d::デバイスの初期化に失敗しました。");
                     return;
                 }
-                using (SimpleLiteD3d sample = new SimpleLiteD3d())
+                
+                using (SimpleLiteD3d sample = new SimpleLiteD3d(frm,nyar_res))
                 {
-                    try{
-                        dev_adapter.Init(frm.ClientSize, sample);
-                    }catch (Exception e){
-                        MessageBox.Show(e.Message,"失敗しちゃった☆");
-                        return;
-                    }
 
-                    // アプリケーションの初期化
-                    if (sample.InitializeApplication(frm, dev_adapter))
+
+                    // メインフォームを表示
+                    frm.Show();
+                    //キャプチャ開始
+                    sample.start();
+                    Stopwatch sw = new Stopwatch();
+                    // フォームにフォーカスがある間はループし続ける
+                    while (frm.Focused)
                     {
-                        // メインフォームを表示
-                        frm.Show();
-                        //キャプチャ開始
-                        sample.StartCap();
-                        Stopwatch sw = new Stopwatch();
-                        // フォームにフォーカスがある間はループし続ける
-                        while (frm.Focused)
-                        {
-                            sw.Start();
-                            // メインループ処理を行う
-                            sample.MainLoop();
-                            //スレッドスイッチ
-                            Thread.Sleep(0);
+                        sw.Start();
+                        // メインループ処理を行う
+                        sample.MainLoop();
+                        //スレッドスイッチ
+                        Thread.Sleep(0);
 
 
 
-                            // イベントがある場合はその処理する
-                            Application.DoEvents();
-                            sw.Stop();
-                            sample.fps_x_100 = (int)(1000 * 100 / (sw.ElapsedMilliseconds+1));
-                            sw.Reset();
+                        // イベントがある場合はその処理する
+                        Application.DoEvents();
+                        sw.Stop();
+                        //sample.fps_x_100 = (int)(1000 * 100 / (sw.ElapsedMilliseconds+1));
+                        sw.Reset();
 
-                        }
-                        //キャプチャの停止
-                        sample.StopCap();
                     }
-                    else
-                    {
-                        // 初期化に失敗
-                    }
-                    dev_adapter.Finish();
+                    //キャプチャの停止
+                    sample.stop();
                 }
             }
+            return;
         }
     }
 }
