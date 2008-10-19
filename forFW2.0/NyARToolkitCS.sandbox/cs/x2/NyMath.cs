b3066d5ead6222bc415b5a7e84f5920368a91800
nyatla <nyatla@7cac0a50-4618-4814-88d0-24b83990f816>
2008-10-19 12:11:18 +0000

    [バックアップ]NyARToolkitCS NyARMathのバグフィクス
    git-svn-id: https://svn.sourceforge.jp/svnroot/nyartoolkit/NyARToolkitCS/trunk@197 7cac0a50-4618-4814-88d0-24b83990f816

--- forFW2.0/NyARToolkitCS.sandbox/cs/x2/NyMath.cs
+++ forFW2.0/NyARToolkitCS.sandbox/cs/x2/NyMath.cs
@@ -37,148 +37,182 @@ namespace jp.nyatla.nyartoolkit.cs.sandbox.x2
 {
     public class NyMath
     {
-        public static long FIXEDFLOAT24_1 = 0x1000000L;
-        public static long FIXEDFLOAT24_0_25 = FIXEDFLOAT24_1 / 4;
-        public static long FIXEDFLOAT16_1 = 0x10000L;
-        private static int FF16_PI = (int)(Math.PI * FIXEDFLOAT16_1);
-        private static int FF16_2PI = (int)(2 * FF16_PI);
-        private static int FF16_05PI = (int)(FF16_PI / 2);
-        private static int SIN_RESOLUTION = 1024;
-        private static int ACOS_RESOLUTION = 256;
-        /* sinテーブルは0-2PIを1024分割
-         * acosテーブルは0-1を256分割
-         */
-        private static int[] sin_table = new int[SIN_RESOLUTION];
-        private static int[] acos_table = new int[ACOS_RESOLUTION + 1];
-        private static int SQRT_LOOP = 10;
-        /**
-         * http://www.geocities.co.jp/SiliconValley-PaloAlto/5438/
-         * 参考にしました。
-         * 少数点部が16bitの変数の平方根を求めます。
-         * 戻り値の小数点部分は16bitです。
-         * @param i_v
-         * @return
-         */
-        public static long sqrtFixdFloat16(long i_ff16)
-        {
-            long t = 0, s;
-            s = i_ff16 > 0 ? i_ff16 : -i_ff16;
-            if (i_ff16 == 0)
-            {
-                return 0;
-            }
-            for (int i = SQRT_LOOP; i > 0; i--)
-            {
-                t = s;
-                s = (t + ((i_ff16 << 16) / t)) >> 1;
-                if (s == t)
-                {
-                    break;
-                }
-            };
-            return t;
-        }
-        public static long sqrtFixdFloat(long i_ff, int i_bit)
-        {
-            long t = 0, s;
-            s = i_ff > 0 ? i_ff : -i_ff;
-            if (i_ff == 0)
-            {
-                return 0;
-            }
-            for (int i = SQRT_LOOP; i > 0; i--)
-            {
-                t = s;
-                s = (t + ((i_ff << i_bit) / t)) >> 1;
-                if (s == t)
-                {
-                    break;
-                }
-            }
-            return t;
-        }
-        public static int acosFixedFloat16(int i_ff24)
-        {
-            int abs_ff24 = i_ff24 > 0 ? i_ff24 : -i_ff24;
-            if (abs_ff24 < FIXEDFLOAT24_0_25)
-            {
-                //0.25までの範囲は、一次の近似式
-                return FF16_05PI - (i_ff24 >> 8);
-            }
-            else
-            {
-                // 0～1を0～512に変換
-                int idx = (int)(i_ff24 >> 16);//S8
-                if (idx < 0)
-                {
-                    return FF16_PI - acos_table[-idx];
-                }
-                else
-                {
-                    return acos_table[idx];
-                }
-            }
-        }
-        public static int sinFixedFloat24(int i_ff16)
-        {
-            // 0～2PIを0～1024に変換
-            int rad_index = (int)(i_ff16 * SIN_RESOLUTION / FF16_2PI);
-            rad_index = rad_index % SIN_RESOLUTION;
-            if (rad_index < 0)
-            {
-                rad_index += SIN_RESOLUTION;
-            }
-            // ここで0-1024にいる
-            return sin_table[rad_index];
-        }
-        public static int cosFixedFloat24(int i_ff16)
-        {
-            // 0～Math.PI/2を 0～256の値空間に変換
-            int rad_index = (int)(i_ff16 * SIN_RESOLUTION / FF16_2PI);
-            // 90度ずらす
-            rad_index = (rad_index + SIN_RESOLUTION / 4) % SIN_RESOLUTION;
-            // 負の領域に居たら、+1024しておく
-            if (rad_index < 0)
-            {
-                rad_index += SIN_RESOLUTION;
-            }
-            // ここで0-1024にいる
-            return sin_table[rad_index];
-            //		return (int)(Math.cos((double)i_ff16/0x10000)*0x1000000);
-        }
-        public static void initialize()
-        {
-            //解像度は4の倍数で無いとダメ
-            Debug.Assert(SIN_RESOLUTION % 4 == 0);
+	public const long FIXEDFLOAT24_1=0x1000000L;
+	public const long FIXEDFLOAT24_0_25=FIXEDFLOAT24_1/4;
+	public const long FIXEDFLOAT16_1=0x10000L;	
+	public const long FIXEDFLOAT16_0_25=FIXEDFLOAT16_1/4;	
+	public const long FIXEDFLOAT8_1=0x100L;
+	
+	private const int FIXEDFLOAT16I_1=(int)FIXEDFLOAT16_1;	
+	private const int FIXEDFLOAT16I_0_25=(int)FIXEDFLOAT16_1/4;	
+	
+	
+	
 
-            int d4 = SIN_RESOLUTION / 4;
-            //sinテーブル初期化(0-2PIを0-1024に作成)
-            for (int i = 1; i < SIN_RESOLUTION; i++)
-            {
-                sin_table[i] = (int)((Math.Sin(2 * Math.PI * (double)i / (double)SIN_RESOLUTION)) * FIXEDFLOAT24_1);
-            }
-            sin_table[0] = 0;
-            sin_table[d4 - 1] = 0x1000000;
-            sin_table[d4 * 2 - 1] = 0;
-            sin_table[d4 * 3 - 1] = -0x1000000;
-            //acosテーブル初期化
-            for (int i = 1; i < ACOS_RESOLUTION; i++)
-            {
-                acos_table[i] = (int)((Math.Acos((double)i / (double)ACOS_RESOLUTION)) * FIXEDFLOAT16_1);
-            }
-            acos_table[0] = FF16_PI;
-            acos_table[ACOS_RESOLUTION] = 0;
-            return;
-        }
-        public static void printF16(long i_value)
-        {
-            Debug.WriteLine((double)i_value / 0x10000);
-            return;
-        }
-        public static void printF24(long i_value)
-        {
-            Debug.WriteLine((double)i_value / 0x1000000);
-            return;
-        }
+	private const int FF16_PI=(int)(Math.PI*FIXEDFLOAT16_1);
+	private const int FF16_2PI=(int)(2 *FF16_PI);
+	private const int FF16_05PI=(int)(FF16_PI/2);
+	/* sinテーブルは0-2PIを1024分割
+	 * acosテーブルは0-1を256分割
+	 */
+	private static int[] sin_table=new int[339];
+	private static int[] acos_table=new int[1537];
+    private const int SQRT_LOOP = 10;
+	/**
+	 * http://www.geocities.co.jp/SiliconValley-PaloAlto/5438/
+	 * 参考にしました。
+	 * 少数点部が16bitの変数の平方根を求めます。
+	 * 戻り値の小数点部分は16bitです。
+	 * @param i_v
+	 * @return
+	 */
+	public static long sqrtFixdFloat16(long i_ff16)
+	{
+		long t=0,s;
+		s=i_ff16>0?i_ff16:-i_ff16;
+		if(i_ff16==0){
+			return 0;
+		}
+		for(int i=SQRT_LOOP;i>0;i--){
+			t = s;
+			s = (t+((i_ff16<<16)/t))>>1;
+			if(s==t){
+				break;
+			}
+		};
+		return t;
+	}
+	public static long sqrtFixdFloat(long i_ff,int i_bit)
+	{
+		long t=0,s;
+		s=i_ff>0?i_ff:-i_ff;
+		if(i_ff==0){
+			return 0;
+		}
+		for(int i=SQRT_LOOP;i>0;i--){
+			t = s;
+			s = (t+((i_ff<<i_bit)/t))>>1;
+			if(s==t){
+				break;
+			}
+		}
+		return t;
+	}
+	public static int acosFixedFloat16(int i_ff24)
+	{/*	
+		long x=i_ff24>>8;
+		long x2=(x*x)>>16;
+		long x3=(x2*x)>>16;
+		long x4=(x2*x2)>>16;
+//		return FF16_05PI-(int)(x+x3/6+(((3*x3*x2/(2*4*5)+(3*5*x4*x3)/(2*4*6*7)))>>16));
+*/		
+		int result;
+		int abs_ff24=i_ff24>0?i_ff24:-i_ff24;
+		//(0<=n<=0.25) 0<=0-16384(65536/4)まではy=PI/2-xので近似
+		//(0.25<n<=1/2PI) 16385-65536までは(128ステップ単位)のテーブルを使用				
+
+		if(abs_ff24<FIXEDFLOAT24_0_25){
+			//0.25までの範囲は、2次の近似式
+			result=(i_ff24>>8);
+			return FF16_05PI-result+((((result*result)>>16)*result)>>16)/6;
+		}else{
+			result=acos_table[((abs_ff24>>8)-FIXEDFLOAT16I_0_25)>>5];
+			if (i_ff24 < 0) {
+				return FF16_PI-result;
+			}else{
+				return result;
+			}
+		}
+//		return (int)(Math.acos((double)i_ff24/0x1000000)*0x10000);
+	}
+
+	public static int sinFixedFloat24(int i_ff16)
+	{
+		int result;
+		//i_ff16を0-2πに制限
+		int rad=i_ff16%FF16_2PI;
+		if(rad<0){
+			rad=rad+FF16_2PI;
+		}
+		//4ブロックに分割
+		int dv=rad/FF16_05PI;
+		//radを0-0.5PIに制限
+		rad=rad-dv*FF16_05PI;
+		//radをdvにより補正
+		if(dv==1 || dv==3){
+			rad=FF16_05PI-rad;
+		}
+		//(0<=n<=0.25) 0<=0-16384(65536/4)まではy=xので近似
+		//(0.25<n<=1/2PI) 16385-102944までは(256ステップ単位)のテーブルを使用				
+		//負にする
+		if(rad<FIXEDFLOAT16_0_25){
+			result=rad<<8;
+		}else{
+			result=sin_table[(rad-FIXEDFLOAT16I_0_25)>>8];
+		}
+		if(dv>=2){
+			result=-result;
+		}
+		return result;
+//		return (int)(Math.sin((double)i_ff16/0x10000)*0x1000000);
+	}
+	public static int cosFixedFloat24(int i_ff16)
+	{
+		int result;
+		//i_ff16を0-2πに制限
+		int rad=(i_ff16+FF16_05PI)%FF16_2PI;
+		if(rad<0){
+			rad=rad+FF16_2PI;
+		}
+		//4ブロックに分割
+		int dv=rad/FF16_05PI;
+		//radを0-0.5PIに制限
+		rad=rad-dv*FF16_05PI;
+		//radをdvにより補正
+		if(dv==1 || dv==3){
+			rad=FF16_05PI-rad;
+		}
+		//(0<=n<=0.25) 0<=0-16384(65536/4)まではy=xので近似
+		//(0.25<n<=1/2PI) 16385-102944までは(256ステップ単位)のテーブルを使用				
+		//負にする
+		if(rad<FIXEDFLOAT16_0_25){
+			result=rad<<8;
+		}else{
+			result=sin_table[(rad-FIXEDFLOAT16I_0_25)>>8];
+		}
+		if(dv>=2){
+			result=-result;
+		}
+		return result;
+//		return (int)(Math.cos((double)i_ff16/0x10000)*0x1000000);
+	}
+	public static void initialize()
+	{
+
+		int step;
+		step=FIXEDFLOAT16I_0_25+256;
+		for(int i=0;i<339;i++){
+			sin_table[i]=(int)((Math.Sin((double) step / (double) FIXEDFLOAT16I_1))*FIXEDFLOAT24_1);
+			step+=256;
+		}
+		//acosテーブル初期化
+		step=FIXEDFLOAT16I_0_25+32;
+		for (int i = 0; i < 1537; i++) {
+			acos_table[i] =(int)((Math.Acos((double) step/(double) FIXEDFLOAT16I_1))*FIXEDFLOAT16_1);
+			step+=32;
+		}
+		return;
+	}
+/*
+	public static void printF16(long i_value)
+	{
+		System.out.println((double)i_value/0x10000);
+		return;
+	}
+	public static void printF24(long i_value)
+	{
+		System.out.println((double)i_value/0x1000000);
+		return;
+	}*/
     }
 }
