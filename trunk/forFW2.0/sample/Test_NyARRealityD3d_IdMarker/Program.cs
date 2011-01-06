8b65fb5583290d7e694d082f94fc025a4ab283e1
nyatla <nyatla@7cac0a50-4618-4814-88d0-24b83990f816>
2011-01-06 10:13:47 +0000

    git-svn-id: https://svn.sourceforge.jp/svnroot/nyartoolkit/NyARToolkitCS@792 7cac0a50-4618-4814-88d0-24b83990f816

--- trunk/forFW2.0/sample/Test_NyARRealityD3d_IdMarker/Program.cs
+++ trunk/forFW2.0/sample/Test_NyARRealityD3d_IdMarker/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
-
+using NyARToolkitCSUtils.Capture;
+using NyARToolkitCSUtils.Capture.dialog;
 namespace Test_NyARRealityGl_IdMarker
 {
     static class Program
@@ -15,7 +17,55 @@ namespace Test_NyARRealityGl_IdMarker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+
+            //キャプチャデバイスリストを取得
+            CaptureDeviceList capture_device_list = new CaptureDeviceList();
+            if (capture_device_list.count < 1)
+            {
+                MessageBox.Show("キャプチャデバイスが見つかりませんでした。");
+                return;
+            }
+            //キャプチャデバイスを選択してもらう。
+            int cdevice_number = 0;
+            using (CameraSelectDialog frm2 = new CameraSelectDialog())
+            {
+                frm2.ShowDialog(capture_device_list, out cdevice_number);
+            }
+            using (CaptureDevice capture_device = capture_device_list[cdevice_number])
+            {
+                // フォームとメインサンプルクラスを作成
+                using (Form1 frm = new Form1())
+                using (Test_NyARRealityD3d_IdMarker sample = new Test_NyARRealityD3d_IdMarker())
+                {
+                    // アプリケーションの初期化
+                    if (sample.InitializeApplication(frm, capture_device))
+                    {
+                        // メインフォームを表示
+                        frm.Show();
+                        //キャプチャ開始
+                        sample.StartCap();
+                        // フォームが作成されている間はループし続ける
+                        while (frm.Created)
+                        {
+                            // メインループ処理を行う
+                            sample.MainLoop();
+
+                            //スレッドスイッチ
+                            Thread.Sleep(1);
+
+                            // イベントがある場合はその処理する
+                            Application.DoEvents();
+                        }
+                        //キャプチャの停止
+                        sample.StopCap();
+                    }
+                    else
+                    {
+                        // 初期化に失敗
+                    }
+                }
+            }
         }
     }
 }
