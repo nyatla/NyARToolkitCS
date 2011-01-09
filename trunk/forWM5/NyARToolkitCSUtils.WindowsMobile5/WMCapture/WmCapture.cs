74cd95f570e38f4bfcf3888d73640fd8803432f6
nyatla <nyatla@7cac0a50-4618-4814-88d0-24b83990f816>
2011-01-09 17:16:16 +0000

    git-svn-id: https://svn.sourceforge.jp/svnroot/nyartoolkit/NyARToolkitCS@800 7cac0a50-4618-4814-88d0-24b83990f816

---
+++ trunk/forWM5/NyARToolkitCSUtils.WindowsMobile5/WMCapture/WmCapture.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using jp.nyatla.cs.NyWMCapture;
+
+namespace NyARToolkitCSUtils.WMCapture
+{
+    public interface IWmCaptureListener
+    {
+        void onSample(WmCapture i_sender, INySample i_sample);
+    }
+    public class WmCapture : IDisposable,INySampleCB
+    {
+        private INyWMCapture _capture;
+        private IWmCaptureListener _on_sample_event = null;
+        private bool _is_start = false;
+        private bool _vertical_flip;
+        public bool vertical_flip { get { return this._vertical_flip; } } 
+        public WmCapture(Size i_cap_size,bool i_vertical_flip_property)
+        {
+            //キャプチャ作る。
+            NyWMCapture cap = new NyWMCapture();
+            INyWMCapture cap_if = (INyWMCapture)cap;
+            int hr;
+            hr = cap_if.SetCallBack(this);//これInitializeの前にやらないといけないのよね。
+            if (hr != 0)
+            {
+                throw new Exception("cap_if.SetCallBack");
+            }
+            hr = cap_if.SetSize(i_cap_size.Width, i_cap_size.Height);
+            if (hr != 0)
+            {
+                throw new Exception("cap_if.SetSize");
+            }
+            hr = cap_if.Initialize(NyWMCapture.DeviceId_WM5, NyWMCapture.MediaSubType_RGB565, NyWMCapture.PinCategory_PREVIEW);
+            if (hr != 0)
+            {
+                throw new Exception("cap_if.Initialize");
+            }
+            this._capture = cap_if;
+            this._vertical_flip = i_vertical_flip_property;
+            return;
+        }
+        public void start()
+        {
+            Debug.Assert(this._is_start==false);
+            this._capture.Start();
+            this._is_start = true;
+            return;
+        }
+        public void stop()
+        {
+            Debug.Assert(this._is_start == true);
+            this._capture.Stop();
+            this._is_start = false;
+            return;
+        }
+        public void setOnSample(IWmCaptureListener i_handler)
+        {
+            this._on_sample_event=i_handler;
+            return;
+        }
+        public int OnSample(INySample i_sample)
+        {
+            if (this._on_sample_event != null)
+            {
+                this._on_sample_event.onSample(this,i_sample);
+            }
+            return 0;
+        }
+        public void Dispose()
+        {
+            if (this._capture != null)
+            {
+                if (this._is_start)
+                {
+                    this._capture.Stop();
+                }
+                this._capture.Finalize();
+                this._capture = null;
+            }
+            return;
+        }
+    }
+}
+
